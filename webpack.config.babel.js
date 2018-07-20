52dbad846157907cf163c2a940d497be75f300b7
Ingo Fahrentholz <ingof.work@gmail.com>
2018-07-21 01:00:06 +0200

    Add webpackDevServer

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,10 @@
-import webpack from "webpack";
 import path from "path";
 import webpackMerge from "webpack-merge";
 
 import handleSCSS from "./scripts/webpack/scss";
 import handleES6 from "./scripts/webpack/es6";
 import htmlInjector from "./scripts/webpack/html";
+import devServer from "./scripts/webpack/dev-server";
 
 export default (env, argv) => {
   return webpackMerge({
@@ -13,6 +13,7 @@ export default (env, argv) => {
       path: path.resolve(__dirname, "dist/"),
     }
   },
+    devServer(),
     handleSCSS(),
     handleES6(),
     htmlInjector()
